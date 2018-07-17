62c5f929e68604f327c1acf877a40e924d01ee94
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-17 18:38:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,14 +5,7 @@ def prime?(number)
   if number <= 1
     false
   else
-    for n in (2..number-1) do
-      if number == n
-        true
-      else number % n == 0
-        false
-      end
-      
-      (2..n/2).none? { |i| n % i == 0}
+    (2..number/2).none? { |i| number % i == 0}
     end
   end
 end
